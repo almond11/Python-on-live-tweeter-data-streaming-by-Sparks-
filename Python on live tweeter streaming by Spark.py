ee3bc96d9be6bef7318e755ebdd3e28be2bee441
almond11 <disearch11@gmail.com>
2017-12-10 10:09:44 -0800

    pyspark

---
+++ Python on live tweeter streaming by Spark.py
@@ -0,0 +1,41 @@
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import desc
+# Can only run this once. restart your kernel for any errors.
+sc = SparkContext()
+ssc = StreamingContext(sc, 10 )
+sqlContext = SQLContext(sc)
+socket_stream = ssc.socketTextStream("127.0.0.1", 5555)
+lines = socket_stream.window( 20 )
+from collections import namedtuple
+fields = ("tag", "count" )
+Tweet = namedtuple( 'Tweet', fields )
+# Use Parenthesis for multiple lines or use \.
+( lines.flatMap( lambda text: text.split( " " ) ) #Splits to a list
+  .filter( lambda word: word.lower().startswith("#") ) # Checks for hashtag calls
+  .map( lambda word: ( word.lower(), 1 ) ) # Lower cases the word
+  .reduceByKey( lambda a, b: a + b ) # Reduces
+  .map( lambda rec: Tweet( rec[0], rec[1] ) ) # Stores in a Tweet Object
+  .foreachRDD( lambda rdd: rdd.toDF().sort( desc("count") ) # Sorts Them in a DF
+  .limit(10).registerTempTable("tweets") ) ) # Registers to a table.
+  ssc.start()    
+  import time
+from IPython import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Only works for Jupyter Notebooks!
+%matplotlib inline 
+count = 0
+while count < 10:
+    
+    time.sleep( 3 )
+    top_10_tweets = sqlContext.sql( 'Select tag, count from tweets' )
+    top_10_df = top_10_tweets.toPandas()
+    display.clear_output(wait=True)
+    sns.plt.figure( figsize = ( 10, 8 ) )
+    sns.barplot( x="count", y="tag", data=top_10_df)
+    sns.plt.show()
+    count = count + 1
+    ssc.stop()
